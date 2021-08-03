6d02fe59fb30f852cc8f8d74bca767351719ceb3
Josh Armentano <josh@abidewebdesign.com>
2021-08-03 16:33:39 -0700

    no message

--- template-parts/blocks/content-gallery.php
+++ template-parts/blocks/content-gallery.php
@@ -1,56 +1,56 @@
-<?php if ( $images = get_field('gallery') ): ?>
+<?php if ( get_field('gallery_type') == 'content-single' ): ?>
+	
+	<?php $images = get_field('gallery'); ?>
+	
+	<?php $type = 'slide-sm'; ?>
 
-	<?php if ( get_field('gallery_type') == 'content-single' ): ?>
-	
-		<?php $type = 'slide-sm'; ?>
-	
-	<?php else: ?>
-	
-		<?php $type = 'slide-lg'; ?>
-		
-	<?php endif; ?>
+<?php else: ?>
 
-	<div class="wrapper-gallery <?php echo ( get_field('gallery_type') == 'content-single' ? 'pb-3' : ''); ?>">
+	<?php $images = get_field('gallery_inline_text'); ?>
+
+	<?php $type = 'slide-lg'; ?>
+	
+<?php endif; ?>
+
+<div class="wrapper-gallery <?php echo ( get_field('gallery_type') == 'content-single' ? 'pb-3' : ''); ?>">
+	
+	<div class="container">
 		
-		<div class="container">
+		<div class="row">
 			
-			<div class="row">
+			<div class="col-12">
 				
-				<div class="col-12">
+				<div id="carousel" class="carousel slide d-flex" data-ride="carousel">
 					
-					<div id="carousel" class="carousel slide d-flex" data-ride="carousel">
+					<?php if ( count( $images ) > 1 ): ?>
 						
-						<?php if ( count( $images ) > 1 ): ?>
+						<ol class="carousel-indicators">
 							
-							<ol class="carousel-indicators">
-								
-								<?php for ( $i = 0; $i < count( $images ); ++$i ): ?>
-								
-										<li data-target="#carousel" data-slide-to="<?php echo $i; ?>" <?php echo ( $i == 0 ? 'class = "active"' : '' ); ?>></li>
-								
-								<?php endfor; ?>
+							<?php for ( $i = 0; $i < count( $images ); ++$i ): ?>
 							
-							</ol>
+									<li data-target="#carousel" data-slide-to="<?php echo $i; ?>" <?php echo ( $i == 0 ? 'class = "active"' : '' ); ?>></li>
 							
-						<?php endif; ?>
+							<?php endfor; ?>
 						
-						<div class="carousel-inner" role="listbox">
-									
-							<?php $x = 0; ?>
+						</ol>
+						
+					<?php endif; ?>
+					
+					<div class="carousel-inner" role="listbox">
 								
-							<?php foreach ( $images as $image ): ?>
-									
-								<div class="carousel-item <?php echo ( $x == 0 ? 'active' : '' ); ?>">
-									
-									<?php echo wp_get_attachment_image( $image, $type, false, array( 'class' => 'img-fluid w-100' ) ); ?>
-									
-								</div>
-								
-								<?php $x ++; ?>
-								
-							<?php endforeach; ?>
+						<?php $x = 0; ?>
 							
-						</div>
+						<?php foreach ( $images as $image ): ?>
+								
+							<div class="carousel-item <?php echo ( $x == 0 ? 'active' : '' ); ?>">
+								
+								<?php echo wp_get_attachment_image( $image, $type, false, array( 'class' => 'img-fluid w-100' ) ); ?>
+								
+							</div>
+							
+							<?php $x ++; ?>
+							
+						<?php endforeach; ?>
 						
 					</div>
 					
@@ -61,5 +61,5 @@
 		</div>
 		
 	</div>
-
-<?php endif; ?>
+	
+</div>
