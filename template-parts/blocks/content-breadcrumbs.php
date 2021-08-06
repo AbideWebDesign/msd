be3748e495c0a9a13251cccedadf2d1c8682038f
Josh Armentano <josh@abidewebdesign.com>
2021-08-06 10:18:38 -0700

    no message

--- template-parts/blocks/content-breadcrumbs.php
+++ template-parts/blocks/content-breadcrumbs.php
@@ -2,7 +2,7 @@
 
 if ( function_exists( 'yoast_breadcrumb' ) ) {
 	
-	yoast_breadcrumb( '<div class="wrapper-breadcrumbs mb-2 mb-lg-0 text-sm d-none d-lg-block">','</div>' );
+	yoast_breadcrumb( '<div class="wrapper-breadcrumbs mb-2 text-sm d-none d-lg-block">','</div>' );
 
 }
 
