f3237d804d76142ddc68a30a1231539835242b4d
Josh Armentano <josh@abidewebdesign.com>
2021-10-30 12:22:45 -0700

    no message

--- footer.php
+++ footer.php
@@ -190,7 +190,7 @@ defined( 'ABSPATH' ) || exit;
 			
 			<div class="col-md-auto mb-1 mb-md-0">
 				
-				<a href="<?php echo home_url('/web-accessibility'); ?>" class="text-white"><?php _e('Web Accessibility'); ?></a> | <a href="<?php echo home_url('/login'); ?>" class="text-white"> <?php _e('Website Login'); ?></a>
+				<a href="<?php echo home_url('/web-accessibility'); ?>" class="text-white"><?php _e('Web Accessibility'); ?></a> | <a href="<?php echo home_url('/wp-login.php'); ?>" class="text-white"> <?php _e('Website Login'); ?></a>
 				
 			</div>
 			
