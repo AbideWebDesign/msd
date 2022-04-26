564d946f8353acca8ad601ca88728ac3706b96f1
Josh Armentano <josh@abidewebdesign.com>
2022-04-26 11:17:28 -0700

    no message

--- inc/setup.php
+++ inc/setup.php
@@ -351,7 +351,9 @@ function admin_bar_style_override() {
 			#your-profile h2, .gswpts-review-notice, .gswpts-affiliate-notice {
 				display: none !important;
 			}
-			
+			#link-selector label {
+				display: inline !important;
+			}
 	<?php
 		
 	}
