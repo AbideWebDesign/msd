276cac5af2a845f306fc9da56ac2fbbad5be2c05
Josh Armentano <josh@abidewebdesign.com>
2022-09-26 15:58:02 -0700

    no message

--- inc/setup.php
+++ inc/setup.php
@@ -340,7 +340,7 @@ function admin_bar_style_override() {
 		
 		<style>
 		
-			#duplicate-post-notice, .acf-to-rest-api-donation-notice, #directory-categorydiv, .ac-message, #ac-pro-version, #direct-feedback, .installer-plugin-update-tr, .plugins .dashicons, .shortpixel-notice, #emr-news, .wrap.emr_upload_form .option-flex-wrapper, .emr_upload_form #message, .user-syntax-highlighting-wrap {
+			#litespeed_meta_boxes, #duplicate-post-notice, .acf-to-rest-api-donation-notice, #directory-categorydiv, .ac-message, #ac-pro-version, #direct-feedback, .installer-plugin-update-tr, .plugins .dashicons, .shortpixel-notice, #emr-news, .wrap.emr_upload_form .option-flex-wrapper, .emr_upload_form #message, .user-syntax-highlighting-wrap {
 				display: none !important;
 			}
 			.sbi_notice, .yoast-notification, #toplevel_page_searchwp-settings, .post-type-alert .view, .post-type-alert #wp-admin-bar-view, .post-type-alert #minor-publishing-actions, #toplevel_page_searchwp-settings, form#your-profile > h3, form#your-profile .user-profile-picture, form#your-profile .user-description-wrap, form#your-profile .user-display-name-wrap, form#your-profile .user-nickname-wrap, form#your-profile .show-admin-bar, .user-comment-shortcuts-wrap, form#your-profile .yoast-settings, form#your-profile .user-rich-editing-wrap, form#your-profile .user-admin-color-wrap, form#your-profile .user-url-wrap, form#your-profile .user-facebook-wrap, form#your-profile .user-instagram-wrap, form#your-profile .user-linkedin-wrap, form#your-profile .user-myspace-wrap, form#your-profile .user-pinterest-wrap, form#your-profile .user-soundcloud-wrap, form#your-profile .user-tumblr-wrap, form#your-profile .user-twitter-wrap, form#your-profile .user-youtube-wrap, form#your-profile .user-wikipedia-wrap  {
