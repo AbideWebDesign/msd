89d0d2a3e14cdc7f3e8c069e607ff861d74eeb01
Josh Armentano <josh@abidewebdesign.com>
2024-08-08 11:09:01 -0700

    no message

--- page-calendar.php
+++ page-calendar.php
@@ -67,9 +67,9 @@
 					
 					<?php endif; ?>
 					
-					<?php if ( get_field('calendar_download_next_copy', 'options') ): ?>
+					<?php if ( get_field('calendar_download_next_sp', 'options') ): ?>
 					
-						<li class="mb-1"><span class="fa-li"><i class="fas fa-chevron-right text-primary"></i></span><a title="Download calendar" href="<?php the_field('calendar_download_next_copy', 'options'); ?>" target="_blank"><?php the_field('calendar_next_year_label', 'options'); ?> <?php _e('Calendar (SP)'); ?></a>
+						<li class="mb-1"><span class="fa-li"><i class="fas fa-chevron-right text-primary"></i></span><a title="Download calendar" href="<?php the_field('calendar_download_next_sp', 'options'); ?>" target="_blank"><?php the_field('calendar_next_year_label', 'options'); ?> <?php _e('Calendar (SP)'); ?></a>
 					
 					<?php endif; ?>
 					
