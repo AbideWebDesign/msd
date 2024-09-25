c2cfecd6e5c0caa2d8fd04b153e87a60eb61ba38
Josh Armentano <josh@abidewebdesign.com>
2024-09-25 09:51:17 -0700

    no message

--- sidebar.php
+++ sidebar.php
@@ -1,74 +1,41 @@
 <?php
-
+	
 global $post;
 
-$exclude = implode(', ', get_field('excluded_pages', 'options') );
+// Initialize variables for child and parent pages
+$exclude = implode( ', ', get_field( 'excluded_pages', 'options' ) );
+$parent_id = $post->post_parent ? $post->post_parent : $post->ID;
+$child_pages_array = [];
 
+// Check if the current page has children for both 'page' and 'np-redirect' post types
 if ( $post->post_parent ) {
+    $child_query = new WP_Query( array(
+        'post_type' => array( 'page', 'np-redirect' ),
+        'post_parent' => $post->ID,
+        'exclude' => $exclude,
+        'posts_per_page' => -1,
+        'orderby' => 'menu_order',
+        'order' => 'ASC'
+    ) );
 
-	$parents = get_post_ancestors( $post->ID );
-
-  	if ( empty( $parents ) || count( $parents ) == 1 ) {
-	  	
-  		$root_page_id = $post->post_parent;
-  		
-  	} else {
-	  	
-  		$x = $post->ancestors;
-
-  		end( $x );
-
-  		$root_page_id = prev( $x );
-  		
-  	}
-  		
-	$walker = new Razorback_Walker_Page_Selective_Children();
-	
-	$children = wp_list_pages( array (
-	    'title_li' => '',
-	    'child_of' => $root_page_id,
-	    'walker' => $walker,
-	    'echo'	=> 0,
-	    'depth' => 1,
-	    'exclude' => $exclude,
-	) );
-	
-	$titlenamer = get_the_title( $root_page_id );
-
-} elseif ( ! empty( get_pages( [ 'child_of' => get_queried_object_id() ] ) ) ) {
-	
-	$children = wp_list_pages( array (
-		'title_li' => '',
-		'depth' => 1,
-		'child_of' => $post->ID,
-		'echo' => 0,
-		'exclude' => $exclude,
-	) );
-	
-	$root_page_id = $post->ID;
-	
-	$titlenamer = get_the_title( $root_page_id );
-
-} else {
-	
-	$query_args = array(
-	    'title_li' => '',
-	    'parent' => 0,
-	    'echo' => 0,
-	    'exclude' => $exclude,
-	    'sort_column' => 'post_name',
-	);
-	
-	$children = wp_list_pages( $query_args );
-	
-	$titlenamer = "Explore More";
-	
+    $child_pages_array = $child_query->posts;
 }
 
-?>
+// Get top-level children of both 'page' and 'np-redirect' types, ordered by menu order
+$top_level_query = new WP_Query( array(
+    'post_type' => array( 'page', 'np-redirect' ),
+    'post_parent' => $parent_id,
+    'exclude' => $exclude,
+    'posts_per_page' => -1,
+    'orderby' => 'menu_order',
+    'order' => 'ASC'
+) );
 
-<?php if ( $children ): ?>
-								
+$top_level_children = $top_level_query->posts;
+
+// Output the sidebar menu structure
+if ( ! empty( $top_level_children ) ): ?>
+
 	<div class="wrapper-sidebar-links bg-light navbar-expand-lg">
 		
 		<div class="bg-blue-dark text-white p-2">
@@ -79,7 +46,7 @@ if ( $post->post_parent ) {
 					
 					<?php if ( ! is_admin() ): ?>
 					
-						<a href="<?php the_permalink( $root_page_id ); ?>"><div class="font-weight-bold text-lg"><?php echo $titlenamer; ?></div></a>
+						<a href="<?php the_permalink( $parent_id ); ?>"><div class="font-weight-bold text-lg"><?php echo get_the_title( $parent_id ); ?></div></a>
 						
 					<?php else: ?>
 					
@@ -108,21 +75,41 @@ if ( $post->post_parent ) {
 		<div class="navbar-collapse collapse sidebar-navbar-collapse">
 			
 			<ul class="nav navbar-nav list-unstyled text-sm mb-0">
-				
+			
 				<?php if ( ! is_admin() ): ?>
 				
-					<?php echo $children; ?>
-					
-				<?php else: ?>
-				
-					<?php _e('Links will go here'); ?>
-					
-				<?php endif; ?>
-				
-			</ul>
+			        <?php foreach ( $top_level_children as $child ): ?>
+			            <li<?php echo $child->ID == $post->ID && ! empty( $child_pages_array ) ? ' class="current_page_item"' : ''; ?>>
+			                <a href="<?php echo ( get_post_type( $child->ID ) == 'np-redirect' ) ? get_post_field( 'post_content', $child->ID ) : get_the_permalink( $child->ID ); ?>" <?php echo ( get_post_type( $child->ID ) == 'np-redirect' ) ? 'target="_blank"' : ''; ?>>
+			                    <?php echo get_the_title( $child->ID ); ?>
+			                </a>
 			
+			                <?php
+			                // Check if the current child page is the one being viewed and if it has children
+			                if ( $child->ID == $post->ID && ! empty( $child_pages_array ) ): ?>
+			                    <ul class="children">
+			                        <?php foreach ( $child_pages_array as $sub_child ): ?>
+			                            <li>
+			                                <a href="<?php echo ( get_post_type( $sub_child->ID ) == 'np-redirect' ) ? get_post_field( 'post_content', $sub_child->ID ) : get_the_permalink( $sub_child->ID ); ?>" <?php echo ( get_post_type( $sub_child->ID ) == 'np-redirect' ) ? 'target="_blank"' : ''; ?>>
+			                                    <?php echo get_the_title( $sub_child->ID ); ?>
+			                                </a>
+			                            </li>
+			                        <?php endforeach; ?>
+			                    </ul>
+			                <?php endif; ?>
+			            </li>
+			        <?php endforeach; ?>
+			        
+			    <?php else: ?>
+			    
+			    	<li><?php _e('Links will go here'); ?></li> 
+			    
+			    <?php endif; ?>
+			    
+    		</ul>
+    		
 		</div>
 		
 	</div>
-					
+	
 <?php endif; ?>
