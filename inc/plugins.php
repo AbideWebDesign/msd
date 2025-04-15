b2fd9dfaceb5e950c3bd6c498a7fbe1990d752be
Josh Armentano <josh@abidewebdesign.com>
2025-04-15 12:41:48 -0700

    no message

--- inc/plugins.php
+++ inc/plugins.php
@@ -5,28 +5,24 @@ add_filter( 'acf/the_field/allow_unsafe_html', function( $allowed, $selector ) {
     return $allowed;
 }, 10, 2);
 
-// Add search weight to more recently published entries in SearchWP.
-// @link https://searchwp.com/documentation/knowledge-base/add-relevance-weight-date/
 add_filter( 'searchwp\query\mods', function( $mods ) {
 	global $wpdb;
+
+	$weight_adjust = 1;
 
 	$mod = new \SearchWP\Mod();
 	$mod->set_local_table( $wpdb->posts );
 	$mod->on( 'ID', [ 'column' => 'id' ] );
-	$mod->relevance( function( $runtime ) use ( $wpdb ) {
+	$mod->relevance( function( $runtime_mod ) use ( $weight_adjust ) {
+		$alias = $runtime_mod->get_local_table_alias();
 		return "
-			COALESCE( ROUND( ( (
-				UNIX_TIMESTAMP( {$runtime->get_local_table_alias()}.post_date )
-				- (
-					SELECT UNIX_TIMESTAMP( {$wpdb->posts}.post_date )
-					FROM {$wpdb->posts}
-					WHERE {$wpdb->posts}.post_status = 'publish'
-					ORDER BY {$wpdb->posts}.post_date ASC
-					LIMIT 1
-				)
-			) / 86400 ), 0 ), 0 )";
+		( 100 * EXP(
+			( 1 - ABS( (
+				UNIX_TIMESTAMP( {$alias}.post_date )
+				- UNIX_TIMESTAMP( NOW() )
+			) / 86400 ) ) / 100 )
+		* {$weight_adjust} )";
 	} );
-
 	$mods[] = $mod;
 
 	return $mods;
